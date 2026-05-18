541afdd8c4a242c288734763e993abec3967a2e3
GutenBerkk <gutenberk32@gmail.com>
2026-05-18 19:22:26 +0200

    kolecko

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let penize = 100
 let poplatek = 5
+let interval = null
+let velikost = 100
+let aktivniHra = false
 
 function hodMinci() {
   let mince = document.getElementById("mince")
@@ -7,8 +10,14 @@ function hodMinci() {
   let vysledek = document.getElementById("vysledek")
   let tlacitko = document.querySelector("button[onclick='hodMinci()']")
 
+  if (penize < poplatek) {
+    vysledek.innerText = "Nemáš dost peněz!"
+    return
+  }
+
   mince.style.animation = "none"
   void mince.offsetWidth
+  
   let jeHlava = Math.random() <= 0.5
   if (jeHlava) {
     mince.style.animation = "flip-hlava 3s forwards"
@@ -16,9 +25,7 @@ function hodMinci() {
     mince.style.animation = "flip-orel 3s forwards"
   }
 
-  if (tlacitko) {
-    tlacitko.disabled = true
-  }
+  tlacitko.disabled = true
   vysledek.innerText = "Točí se..."
 
   setTimeout(function() {
@@ -31,8 +38,69 @@ function hodMinci() {
     }
     penize -= poplatek
     penizeP.innerText = penize
-    if (tlacitko) {
-      tlacitko.disabled = false
-    }
+    tlacitko.disabled = false
   }, 3000)
 }
+
+function zacniNafukovat() {
+  if (penize < poplatek && !aktivniHra) {
+    document.getElementById("vysledek").innerText = "Nemáš dost peněz!"
+    return
+  }
+
+  if (!aktivniHra) {
+    penize -= poplatek
+    document.getElementById("penize").innerText = penize
+    aktivniHra = true
+  }
+
+  document.getElementById("vysledek").innerText = "Riskuj a drž..."
+
+  interval = setInterval(function() {
+    if (Math.random() < 0.02) {
+      praskni()
+      return
+    }
+
+    velikost += 3
+    document.getElementById("koleckoGame").style.transform = `scale(${velikost / 100})`
+    
+    let moznaVyhra = Math.floor(poplatek * (velikost / 100))
+    document.getElementById("Nasobitel").innerText = `Možná výhra: ${moznaVyhra} Kč`
+  }, 50)
+}
+
+function pustiKolecko() {
+  if (!aktivniHra) return
+
+  clearInterval(interval)
+  
+  let moznaVyhra = Math.floor(poplatek * (velikost / 100))
+  penize += moznaVyhra
+  
+  document.getElementById("penize").innerText = penize
+  document.getElementById("vysledek").innerText = `Vybral jsi výhru ${moznaVyhra} Kč!`
+  
+  resetKolecka()
+}
+
+function praskni() {
+  clearInterval(interval)
+  document.getElementById("koleckoGame").style.display = "none"
+  document.getElementById("vysledek").innerText = "💥 PRASKLO TO!"
+  
+  setTimeout(function() {
+    document.getElementById("koleckoGame").style.display = "block"
+    resetKolecka()
+  }, 2000)
+}
+
+function resetKolecka() {
+  velikost = 100
+  aktivniHra = false
+  document.getElementById("koleckoGame").style.transform = "scale(1)"
+  document.getElementById("Nasobitel").innerText = "Možná výhra: 0 Kč"
+}
+
+window.onmouseup = pustiKolecko
+window.ontouchend = pustiKolecko
