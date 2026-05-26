813bb15fbbab8b793806334aee5aea333325d725
GutenBerkk <gutenberk32@gmail.com>
2026-05-26 21:51:09 +0200

    hopa

--- script.js
+++ script.js
@@ -1,119 +1,199 @@
-let penize = Number(localStorage.getItem("penize")) || 100
-document.getElementById("cash").innerText = penize
+// 1. Inicializace peněz z localStorage (pokud neexistuje, nastaví se 100)
+let penize = Number(localStorage.getItem("penize"));
+if (isNaN(penize) || penize <= 0) {
+  penize = 100;
+  localStorage.setItem("penize", penize);
+}
 
-let poplatek = 5
-let interval = null
-let velikost = 100
-let aktivniHra = false
+// Zobrazení peněz na webu hned po načtení
+document.getElementById("cash").innerText = penize;
 
+// Společné stavové proměnné (DEKLAROVÁNY POUZE JEDNOU)
+let pPoints = 0, dPoints = 0, bSazka = 0, bBezi = false;
+let poplatek = 5;
+let interval = null;
+let velikost = 100;
+let aktivniHra = false;
+
+// Databáze mincí
 let minceData = {
   "10kč": { label: "10 Kč", win: 10, loss: 10, fee: 5, headSrc: "img/head_10.png", tailSrc: "img/tail_10.png" },
   "50kč": { label: "50 Kč", win: 50, loss: 50, fee: 20, headSrc: "img/head_50.png", tailSrc: "img/tail_50.png" },
   "zidcoin": { label: "Židovská koruna", win: 200, loss: 200, fee: 50, headSrc: "img/head_zidcoin.png", tailSrc: "img/tail_zidcoin.png" },
   "goldcoin": { label: "Zlatá mince", win: 500, loss: 500, fee: 100, headSrc: "img/head_goldzid.png", tailSrc: "img/tail_goldzid.png" }
+};
+
+// Pomocná funkce pro uložení a vykreslení peněz (předchází chybám s localStorage)
+function aktualizujZustatek() {
+  document.getElementById("cash").innerText = penize;
+  localStorage.setItem("penize", penize);
 }
 
 function updatecoin() {
-  let vyber = document.getElementById("vyberMince")
-  if (!vyber) return
-  let vybrana = document.getElementById("vybranaMince")
-  let pop = document.getElementById("poplatek")
-  let hlavaImg = document.querySelector("#mince .hlava img")
-  let orelImg = document.querySelector("#mince .orel img")
-  let mince = minceData[vyber.value]
-  vybrana.innerText = mince.label
-  pop.innerText = mince.fee
-  hlavaImg.src = mince.headSrc
-  orelImg.src = mince.tailSrc
+  let vyber = document.getElementById("vyberMince");
+  if (!vyber) return;
+  let vybrana = document.getElementById("vybranaMince");
+  let pop = document.getElementById("poplatek");
+  let hlavaImg = document.querySelector("#mince .hlava img");
+  let orelImg = document.querySelector("#mince .orel img");
+  let mince = minceData[vyber.value];
+  
+  vybrana.innerText = mince.label;
+  pop.innerText = mince.fee;
+  hlavaImg.src = mince.headSrc;
+  orelImg.src = mince.tailSrc;
 }
 
 function hodMinci() {
-  let mince = document.getElementById("mince")
-  let penizeP = document.getElementById("cash")
-  let vysledek = document.getElementById("vysledek")
-  let tlacitko = document.querySelector("button[onclick='hodMinci()']")
-  if (!tlacitko) return
-  let vyber = document.getElementById("vyberMince")
-  let vybranaMinc = minceData[vyber.value]
+  let mince = document.getElementById("mince");
+  let vysledek = document.getElementById("vysledek");
+  let tlacitko = document.querySelector("button[onclick='hodMinci()']");
+  if (!tlacitko) return;
+  
+  let vyber = document.getElementById("vyberMince");
+  let vybranaMinc = minceData[vyber.value];
 
   if (penize < vybranaMinc.fee) {
-    vysledek.innerText = "Nemáš dost peněz!"
-    return
+    vysledek.innerText = "Nemáš dost peněz na poplatek!";
+    return;
   }
 
-  mince.style.animation = "none"
-  void mince.offsetWidth
+  // Odečtení poplatku IHNED před hodem
+  penize -= vybranaMinc.fee;
+  aktualizujZustatek();
+
+  mince.style.animation = "none";
+  void mince.offsetWidth;
   
-  let jeHlava = Math.random() <= 0.5
+  let jeHlava = Math.random() <= 0.5;
   if (jeHlava) {
-    mince.style.animation = "flip-orel 3s forwards"
+    mince.style.animation = "flip-hlava 3s forwards";
   } else {
-    mince.style.animation = "flip-hlava 3s forwards"
+    mince.style.animation = "flip-orel 3s forwards";
   }
 
-  tlacitko.disabled = true
-  vysledek.innerText = "Točí se..."
+  tlacitko.disabled = true;
+  vysledek.innerText = "Točí se...";
 
   setTimeout(function() {
     if (jeHlava) {
-      penize += vybranaMinc.win
-      vysledek.innerText = "Padla hlava, přidáno " + vybranaMinc.win + " Kč."
+      penize += vybranaMinc.win;
+      vysledek.innerText = "Padla hlava, přidáno " + vybranaMinc.win + " Kč.";
     } else {
-      penize -= vybranaMinc.loss
-      vysledek.innerText = "Padl orel, odebráno " + vybranaMinc.loss + " Kč."
+      penize -= vybranaMinc.loss;
+      vysledek.innerText = "Padl orel, odebráno " + vybranaMinc.loss + " Kč.";
     }
-    penize -= vybranaMinc.fee
-    penizeP.innerText = penize
-    tlacitko.disabled = false
-    localStorage.setItem("penize", penize)
-  }, 3000)
+    
+    tlacitko.disabled = false;
+    aktualizujZustatek(); // Uložení výsledku hodu
+  }, 3000);
 }
 
 function ruleta(vybrano) {
-  let padla = Math.random() < 0.5 ? "cerna" : "cervena"
+  let padla = Math.random() < 0.5 ? "cerna" : "cervena";
   
   if (padla === "cerna") {
-    document.getElementById("padla").innerText = "Černá"
+    document.getElementById("padla").innerText = "Černá";
   } else {
-    document.getElementById("padla").innerText = "Červená"
+    document.getElementById("padla").innerText = "Červená";
   }
 
-  let sazka = Number(document.getElementById("sazka").value)
-  let vysledekRuleta = document.getElementById("vysledekRuleta")
+  let sazka = Number(document.getElementById("sazka").value);
+  let vysledekRuleta = document.getElementById("vysledekRuleta");
 
   if (sazka > penize) {
-    vysledekRuleta.innerText = "Nemáš dost peněz na tuto sázku!"
-    return
+    vysledekRuleta.innerText = "Nemáš dost peněz na tuto sázku!";
+    return;
   }
   
   if (sazka <= 0) {
-    vysledekRuleta.innerText = "Sázka musí být kladná!"
-    return
+    vysledekRuleta.innerText = "Sázka musí být kladná!";
+    return;
   }
 
   if (vybrano === padla) {
-    penize += sazka
-    vysledekRuleta.innerText = "Vyhrál jsi " + sazka + " Kč!"
+    penize += sazka;
+    vysledekRuleta.innerText = "Vyhrál jsi " + sazka + " Kč!";
   } else {
     penize -= sazka
-    vysledekRuleta.innerText = "Prohrál jsi " + sazka + " Kč!"
+    vysledekRuleta.innerText = "Prohrál jsi " + sazka + " Kč!";
   }
 
-  document.getElementById("cash").innerText = penize
-  localStorage.setItem("penize", penize)
+  aktualizujZustatek();
 }
 
 function pujcitPenize() {
-  let castka = Number(document.getElementById("pujckaInput").value)
-  let zprava = document.getElementById("zpravaoPujcce")
+  let castka = Number(document.getElementById("pujckaInput").value);
+  let zprava = document.getElementById("zpravaoPujcce");
 
   if (castka <= 0) {
-    zprava.innerText = "Musíš zadat kladnou částku!"
-    return
+    zprava.innerText = "Musíš zadat kladnou částku!";
+    return;
   }
 
-  penize += castka
-  document.getElementById("cash").innerText = penize
-  localStorage.setItem("penize", penize)
-  zprava.innerText = "Úspěšně sis půjčil " + castka + " Kč!"
+  penize += castka;
+  zprava.innerText = "Úspěšně sis půjčil " + castka + " Kč!";
+  aktualizujZustatek();
+}
+
+// SEKCE BLACKJACK
+function startBlackjack() {
+  if (bBezi) return;
+  
+  bSazka = Number(document.getElementById("sazkaBlackjack").value);
+  if (isNaN(bSazka) || bSazka <= 0 || bSazka > penize) {
+    document.getElementById("vysledekBlackjack").innerText = "Chyba sázky!";
+    return;
+  }
+
+  penize -= bSazka;
+  aktualizujZustatek();
+  bBezi = true;
+
+  pPoints = Math.floor(Math.random() * 10) + 2 + Math.floor(Math.random() * 10) + 2;
+  dPoints = Math.floor(Math.random() * 10) + 2;
+  
+  document.getElementById("playerHand").innerText = pPoints;
+  document.getElementById("dealerHand").innerText = dPoints;
+  document.getElementById("vysledekBlackjack").innerText = "Hraješ...";
+
+  if (pPoints === 21) {
+    konecBJ("Blackjack!", 2.5);
+    return; 
+  }
+}
+
+function hit() {
+  if (!bBezi) return;
+  
+  pPoints += Math.floor(Math.random() * 10) + 2;
+  document.getElementById("playerHand").innerText = pPoints;
+  
+  if (pPoints > 21) {
+    konecBJ("Moc! Prohra.", 0);
+  }
+}
+
+function stand() {
+  if (!bBezi) return;
+  
+  while (dPoints < 17) {
+    dPoints += Math.floor(Math.random() * 10) + 2;
+  }
+  document.getElementById("dealerHand").innerText = dPoints;
+
+  if (dPoints > 21 || pPoints > dPoints) {
+    konecBJ("Výhra!", 2);
+  } else if (pPoints < dPoints) {
+    konecBJ("Prohra.", 0);
+  } else {
+    konecBJ("Remíza.", 1);
+  }
+}
+
+function konecBJ(txt, nasobek) {
+  bBezi = false;
+  penize += Math.floor(bSazka * nasobek);
+  document.getElementById("vysledekBlackjack").innerText = txt;
+  aktualizujZustatek();
 }
