7bef6f3c7333f1e2cb45ea016a32dd07daa70d70
skure <skure@users.noreply.github.com>
2026-05-26 20:42:00 +0200

    notfinalmincetotalai

--- script.js
+++ script.js
@@ -7,25 +7,50 @@ let interval = null
 let velikost = 100
 let aktivniHra = false
 
+// Data pro jednotlivé mince
+let minceData = {
+  "10kč": { label: "10 Kč", win: 10, loss: 10, fee: 5, headSrc: "img/head_10.png", tailSrc: "img/tail_10.png" },
+  "50kč": { label: "50 Kč", win: 50, loss: 50, fee: 20, headSrc: "img/head_50.png", tailSrc: "img/tail_50.png" },
+  "zidcoin": { label: "Židovská koruna", win: 200, loss: 200, fee: 50, headSrc: "img/head_zidovska.png", tailSrc: "img/tail_zidovska.png" },
+  "goldcoin": { label: "Zlatá mince", win: 500, loss: 500, fee: 100, headSrc: "img/head_zlata.png", tailSrc: "img/tail_zlata.png" }
+}
+
+function aktualizujVybranouMinci() {
+  let vyber = document.getElementById("vyberMince")
+  if (!vyber) return
+  let vybrana = document.getElementById("vybranaMince")
+  let pop = document.getElementById("poplatek")
+  let hlavaImg = document.querySelector("#mince .hlava img")
+  let orelImg = document.querySelector("#mince .orel img")
+  let mince = minceData[vyber.value]
+  vybrana.innerText = mince.label
+  pop.innerText = mince.fee
+  hlavaImg.src = mince.headSrc
+  orelImg.src = mince.tailSrc
+}
+
 function hodMinci() {
-  let mince = document.getElementById("mince")
-  let penizeP = document.getElementById("cash") // Změněno na cash podle navigace
+  let minceElement = document.getElementById("mince")
+  let penizeP = document.getElementById("cash")
   let vysledek = document.getElementById("vysledek")
   let tlacitko = document.querySelector("button[onclick='hodMinci()']")
+  if (!tlacitko) return
+  let vyber = document.getElementById("vyberMince")
+  let vybranaMinc = minceData[vyber.value]
 
-  if (penize < poplatek) {
+  if (penize < vybranaMinc.fee) {
     vysledek.innerText = "Nemáš dost peněz!"
     return
   }
 
-  mince.style.animation = "none"
-  void mince.offsetWidth
+  minceElement.style.animation = "none"
+  void minceElement.offsetWidth
   
   let jeHlava = Math.random() <= 0.5
   if (jeHlava) {
-    mince.style.animation = "flip-hlava 3s forwards"
+    minceElement.style.animation = "flip-hlava 3s forwards"
   } else {
-    mince.style.animation = "flip-orel 3s forwards"
+    minceElement.style.animation = "flip-orel 3s forwards"
   }
 
   tlacitko.disabled = true
@@ -33,17 +58,38 @@ function hodMinci() {
 
   setTimeout(function() {
     if (jeHlava) {
-      penize -= 10
-      vysledek.innerText = "Padl orel, odebráno 10 Kč."
+      penize += vybranaMinc.win
+      vysledek.innerText = "Padla hlava, přidáno " + vybranaMinc.win + " Kč."
     } else {
-      penize += 20
-      vysledek.innerText = "Padla hlava, přidáno 20 Kč."
+      penize -= vybranaMinc.loss
+      vysledek.innerText = "Padl orel, odebráno " + vybranaMinc.loss + " Kč."
     }
-    penize -= poplatek
+    penize -= vybranaMinc.fee
     penizeP.innerText = penize
     tlacitko.disabled = false
 
-    // 2. Uložení aktuálních peněz po skončení hodu
     localStorage.setItem("penize", penize)
   }, 3000)
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  aktualizujVybranouMinci()
+})
+
+function pujcitPenize() {
+  let input = document.getElementById("pujckaInput")
+  let castka = Number(input.value)
+  let zprava = document.getElementById("zpravaoPujcce")
+  let cashEl = document.getElementById("cash")
+
+  if (!castka || castka <= 0) {
+    zprava.innerText = "Zadej prosím platnou částku."
+    return
+  }
+
+  penize += castka
+  cashEl.innerText = penize
+  zprava.innerText = "Úspěšně si vypůjčeno " + castka + " Kč. Máš teď " + penize + " Kč."
+  localStorage.setItem("penize", penize)
+  input.value = ""
 }
